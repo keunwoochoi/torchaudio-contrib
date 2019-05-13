3ea3c1da58b7a06ed2276a643bf99c28b79bbd8f
Keunwoo Choi <keunwooc@spotify.com>
2019-05-12 23:23:20 -0400

    add stft value test. change var names in tests

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -4,9 +4,13 @@ Test the layers. Currently only on cpu since travis doesn't have GPU.
 import unittest
 import torch
 import torch.nn as nn
+import librosa
+import numpy as np
 from torchaudio_contrib.layers import STFT, ComplexNorm, \
     ApplyFilterbank, Spectrogram, Melspectrogram, MelFilterbank, \
     AmplitudeToDb, DbToAmplitude, MuLawEncoding, MuLawDecoding
+from torchaudio_contrib.functional import magphase
+
 
 
 def _num_stft_bins(signal_len, fft_len, hop_len, pad):
@@ -37,30 +41,52 @@ class Tester(unittest.TestCase):
         Padding: Value in having padding outside of torch.stft?
         """
 
-        def _test_mono_cpu():
+        def _test_mono_sizes_cpu():
             _seed()
-            signal = torch.randn(1, 100000)
+            waveform = torch.randn(1, 100000)
             fft_len, hop_len = 512, 256
             layer = STFT(fft_len=fft_len, hop_len=hop_len)
-            spect = layer(signal)
-            assert spect.size(0) == 1
-            assert spect.size(1) == fft_len // 2 + 1
-            assert spect.size(2) == _num_stft_bins(
-                signal.size(-1), fft_len, hop_len, fft_len // 2)
-            assert spect.dim() == signal.dim() + 2
+            complex_spec = layer(waveform)
+            assert complex_spec.size(0) == 1
+            assert complex_spec.size(1) == fft_len // 2 + 1
+            assert complex_spec.size(2) == _num_stft_bins(
+                waveform.size(-1), fft_len, hop_len, fft_len // 2)
+            assert complex_spec.dim() == waveform.dim() + 2
 
-        def _test_batch_multichannel_cpu():
+        def _test_batch_multichannel_sizes_cpu():
             _seed()
-            signal = torch.randn(1, 2, 100000)
+            waveform = torch.randn(1, 2, 100000)
             fft_len, hop_len = 512, 256
             layer = STFT(fft_len=fft_len, hop_len=hop_len)
-            spect = layer(signal)
+            complex_spec = layer(waveform)
 
-            assert spect.size(1) == signal.size(1)
-            assert spect.dim() == signal.dim() + 2
+            assert complex_spec.size(1) == waveform.size(1)
+            assert complex_spec.dim() == waveform.dim() + 2
 
-        _test_mono_cpu()
-        _test_batch_multichannel_cpu()
+        def _test_values():
+            _seed()
+            waveform = torch.randn(1, 10000)
+            fft_len, hop_len = 512, 256
+            layer = STFT(fft_len=fft_len, hop_len=hop_len)
+            complex_spec_torch = layer(waveform)  # (1, 257, 40, 2)
+            complex_spec_librosa = librosa.stft(waveform.numpy()[0],
+                                                n_fft=fft_len,
+                                                hop_length=hop_len)
+
+            mag_spec_torch, phase_spec_torch = magphase(complex_spec_torch)
+            mag_spec_librosa, phase_spec_librosa = librosa.magphase(complex_spec_librosa)
+
+            mag_spec_torch = mag_spec_torch.numpy()
+
+            complex_spec_torch = complex_spec_torch.numpy()
+            complex_spec_torch = complex_spec_torch[:, :, :, 0] + 1j * complex_spec_torch[:, :, :, 1] # np.complex
+
+            assert np.allclose(mag_spec_torch, mag_spec_librosa, atol=1e-6)
+            assert np.allclose(complex_spec_torch, complex_spec_librosa, atol=1e-5)
+
+        _test_mono_sizes_cpu()
+        _test_batch_multichannel_sizes_cpu()
+        _test_values()
 
     def test_ComplexNorm(self):
         """
@@ -69,13 +95,13 @@ class Tester(unittest.TestCase):
 
         def _test_powers(p):
             _seed()
-            stft = torch.randn(1, 257, 391, 2)
-            layer = ComplexNorm(power=p)
-            spect = layer(stft)
-            manual_spect = stft[0][0][0].pow(2).sum(-1).pow(1 / 2).pow(p)
-            assert stft.shape[1:3] == spect.shape[1:3]
-            assert manual_spect == spect[0][0][0]
-            assert stft.dim() - spect.dim() == 1
+            complex_spec = torch.randn(1, 257, 391, 2)
+            complex_norm = ComplexNorm(power=p)
+            mag_spec = complex_norm(complex_spec)
+            mag_spec_manual = complex_spec[0][0][0].pow(2).sum(-1).pow(1 / 2).pow(p)
+            assert complex_spec.shape[1:3] == mag_spec.shape[1:3]
+            assert mag_spec_manual == mag_spec[0][0][0]
+            assert complex_spec.dim() - mag_spec.dim() == 1
 
         for p in [1., 2., 0.7]:
             _test_powers(p)
@@ -88,27 +114,27 @@ class Tester(unittest.TestCase):
         def _test_mono_cpu():
             _seed()
             new_len = 120
-            spect = torch.randn(1, 257, 391)
-            filterbank = torch.randn(spect.size(-2), new_len)
+            mag_spec = torch.randn(1, 257, 391)
+            filterbank = torch.randn(mag_spec.size(-2), new_len)
 
-            layer = ApplyFilterbank(filterbank)
-            spect2 = layer(spect)
+            apply_filterbank = ApplyFilterbank(filterbank)
+            mag_spec_filterbanked = apply_filterbank(mag_spec)
 
-            assert spect.size(-1) == spect2.size(-1)
-            assert spect2.size(-2) == new_len
-            assert spect.dim() == spect2.dim()
+            assert mag_spec.size(-1) == mag_spec_filterbanked.size(-1)
+            assert mag_spec_filterbanked.size(-2) == new_len
+            assert mag_spec.dim() == mag_spec_filterbanked.dim()
 
         def _test_multichannel_cpu():
             _seed()
             new_len = 120
-            spect = torch.randn(1, 2, 257, 391)
-            filterbank = torch.randn(spect.size(-2), new_len)
+            mag_specs = torch.randn(1, 2, 257, 391)
+            filterbank = torch.randn(mag_specs.size(-2), new_len)
 
-            layer = ApplyFilterbank(filterbank)
-            spect2 = layer(spect)
+            apply_filterbank = ApplyFilterbank(filterbank)
+            mag_specs_filterbanked = apply_filterbank(mag_specs)
 
-            assert spect2.size(-2) == new_len
-            assert spect.dim() == spect2.dim()
+            assert mag_specs_filterbanked.size(-2) == new_len
+            assert mag_specs.dim() == mag_specs_filterbanked.dim()
 
         _test_mono_cpu()
         _test_multichannel_cpu()
@@ -121,18 +147,18 @@ class Tester(unittest.TestCase):
         def _create_toy_model(channels):
             _seed()
             fft_len, hop_len = 512, 256
-            spect_layer = Spectrogram(
+            spectrogram_layer = Spectrogram(
                 fft_len=fft_len, hop_len=hop_len, power=1)
             conv_layer = nn.Conv2d(channels, 16, 3)
-            return nn.Sequential(spect_layer, conv_layer)
+            return nn.Sequential(spectrogram_layer, conv_layer)
 
-        signal = torch.randn(1, 2, 100000)
-        toy_model = _create_toy_model(signal.size(1))
+        waveforms = torch.randn(1, 2, 100000)
+        toy_model = _create_toy_model(waveforms.size(1))
 
         toy_model[1].weight.data.fill_(0)
         sd = toy_model.state_dict()
 
-        toy_model2 = _create_toy_model(signal.size(1))
+        toy_model2 = _create_toy_model(waveforms.size(1))
         toy_model2.load_state_dict(sd)
 
         assert len(sd.keys()) == 2
@@ -168,13 +194,13 @@ class Tester(unittest.TestCase):
 
         def _test_mel_sd():
             _seed()
-            signal = torch.randn(1, 1, 100000)
-            toy_model = _create_toy_model(signal.size(1))
+            waveforms = torch.randn(1, 1, 100000)
+            toy_model = _create_toy_model(waveforms.size(1))
 
             toy_model[1].weight.data.fill_(0)
             sd = toy_model.state_dict()
 
-            toy_model2 = _create_toy_model(signal.size(1))
+            toy_model2 = _create_toy_model(waveforms.size(1))
             toy_model2.load_state_dict(sd)
 
             assert len(sd.keys()) == 2
@@ -183,14 +209,14 @@ class Tester(unittest.TestCase):
         def _test_custom_fb():
             _seed()
             num_bands, sample_rate, fft_len, hop_len = 128, 22050, 512, 256
-            signal = torch.randn(1, 1, 100000)
+            waveforms = torch.randn(1, 1, 100000)
             mel_layer = Melspectrogram(
                 num_bands=num_bands,
                 sample_rate=sample_rate,
                 fft_len=fft_len,
                 hop_len=hop_len,
                 mel_filterbank=TestFilterbank)
-            mel_spect = mel_layer(signal)
+            mel_spect = mel_layer(waveforms)
             assert mel_spect.size(-2) == num_bands
 
         _test_mel_sd()
@@ -238,31 +264,31 @@ class Tester(unittest.TestCase):
             n_quantize = 256
             encoding_layer = MuLawEncoding(n_quantize)
 
-            x = 2 * (torch.rand(1, 1024) - 0.5)  # in [-1, 1)
+            waveform = 2 * (torch.rand(1, 1024) - 0.5)  # in [-1, 1)
             # manual computation
-            mu = torch.tensor(n_quantize - 1, dtype=x.dtype, requires_grad=False)
-            x_mu = x.sign() * torch.log1p(mu * x.abs()) / torch.log1p(mu)
-            x_mu = ((x_mu + 1) / 2 * mu + 0.5).long()
+            mu = torch.tensor(n_quantize - 1, dtype=waveform.dtype, requires_grad=False)
+            waveform_mu = waveform.sign() * torch.log1p(mu * waveform.abs()) / torch.log1p(mu)
+            waveform_mu = ((waveform_mu + 1) / 2 * mu + 0.5).long()
 
-            assert _all_equal(encoding_layer(x),
-                              x_mu)
+            assert _all_equal(encoding_layer(waveform),
+                              waveform_mu)
 
         def _test_mu_decoding():
             _seed()
             n_quantize = 256
             decoding_layer = MuLawDecoding(n_quantize)
 
-            x_mu = torch.randint(low=0, high=n_quantize - 1,
+            waveform_mu = torch.randint(low=0, high=n_quantize - 1,
                                  size=(1, 1024))
 
             # manual computation
-            x_mu = x_mu.to(torch.float)
-            mu = torch.tensor(n_quantize - 1, dtype=x_mu.dtype, requires_grad=False)  # confused about dtype here..
-            x = (x_mu / mu) * 2 - 1.
-            x = x.sign() * (torch.exp(x.abs() * torch.log1p(mu)) - 1.) / mu
+            waveform_mu = waveform_mu.to(torch.float)
+            mu = torch.tensor(n_quantize - 1, dtype=waveform_mu.dtype, requires_grad=False)  # confused about dtype here..
+            waveform = (waveform_mu / mu) * 2 - 1.
+            waveform = waveform.sign() * (torch.exp(waveform.abs() * torch.log1p(mu)) - 1.) / mu
 
-            assert _all_equal(decoding_layer(x_mu),
-                              x)
+            assert _all_equal(decoding_layer(waveform_mu),
+                              waveform)
 
         def _test_both_ways():
             _seed()
@@ -270,10 +296,10 @@ class Tester(unittest.TestCase):
             encoding_layer = MuLawEncoding(n_quantize)
             decoding_layer = MuLawDecoding(n_quantize)
 
-            x_mu = torch.randint(low=0, high=n_quantize - 1,
+            waveform_mu = torch.randint(low=0, high=n_quantize - 1,
                                  size=(1, 1024))
-            assert _all_equal(x_mu,
-                              encoding_layer(decoding_layer(x_mu)))
+            assert _all_equal(waveform_mu,
+                              encoding_layer(decoding_layer(waveform_mu)))
 
         _test_mu_encoding()
         _test_mu_decoding()
