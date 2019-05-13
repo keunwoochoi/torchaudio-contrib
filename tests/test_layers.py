ad747e98ecd9495d57549565e34d19f356600d36
Keunwoo Choi <keunwooc@spotify.com>
2019-05-13 00:34:54 -0400

    what if load librosa first

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,6 +1,9 @@
 """
 Test the layers. Currently only on cpu since travis doesn't have GPU.
 """
+from librosa import stft as librosa_stft
+from librosa import magphase as librosa_magphase
+
 import torch
 import torch.nn as nn
 from torchaudio_contrib.layers import STFT, ComplexNorm, \
@@ -60,8 +63,8 @@ class Tester(unittest.TestCase):
             assert complex_spec.dim() == waveform.dim() + 2
 
         def _test_values():
-            from librosa import stft as librosa_stft
-            from librosa import magphase as librosa_magphase
+            # from librosa import stft as librosa_stft
+            # from librosa import magphase as librosa_magphase
             # from numpy import allclose as np_allclose
             # from torchaudio_contrib.functional import magphase
             #
